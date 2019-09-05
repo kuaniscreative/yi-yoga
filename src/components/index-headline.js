4a5f617e339d4d3eba6ec1155257e57707f72212
Kuanisinajar <superbenben@livemail.tw>
2019-09-05 13:50:56 +0800

    Added redux package

---
+++ src/components/index-headline.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const IndexHeadline = () => {
+    return (
+        <div>
+            This is the title of Index
+        </div>
+    )
+}
+
+export default IndexHeadline
