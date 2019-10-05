8fca3c755b69d10942f1baacb37731d3811abfb3
Kuanisinajar <superbenben@livemail.tw>
2019-10-05 15:29:27 +0800

    Finished signIn form and signIn functions

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+// actions
+import { signIn } from '../actions/auth';
 
 class LogIn extends Component {
-    handleSubmit = () => {
-        console.log('sending Account and password');
+
+    state = {
+        email: null,
+        password: null
     }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.signIn(this.state)
+    }
+
     render() {
+        console.log(this.props);
         return (
             <div>
                 登入
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>帳號</label>
-                    <input type="text"/>
+                    <input name='email' type="text" onChange={this.handleChange} />
                     <label>密碼</label>
-                    <input type="text"/>
-                    <a href="#" onClick={ this.handleSubmit }>送出</a>
+                    <input name='password' type="password" onChange={this.handleChange} />
+                    <button>送出</button>
                 </form>
                 <Link to='/'>取消</Link>
             </div>
@@ -22,4 +42,18 @@ class LogIn extends Component {
     }
 }
 
-export default LogIn
+const mapStateToProps = (state) => {
+    return {
+        test: state.firebase
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signIn: (cred) => {
+            dispatch(signIn(cred))
+        } 
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn)
