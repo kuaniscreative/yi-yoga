e091b061606d3a887623970d7ba67842ee46a833
Kuanisinajar <superbenben@livemail.tw>
2019-10-14 10:45:40 +0800

    Added f2e registerClassProcessor

--- src/components/users/userPanel.js
+++ src/components/users/userPanel.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
 
 // actions
 import { signOut } from '../../actions/authActions';
 
 class UserPanel extends Component {
     render() {
+
+        const registerClassProcessor = this.props.newSession ? <Link to='/register-session'>報名</Link> : null;
+
         return (
             <div>
                 <div>歡迎xxx</div>
                 <Link to="/leave-application">請假</Link>
                 <Link to="/reschedule">補課</Link>
+                { registerClassProcessor }
                 <button onClick={this.props.signOut}>登出</button>
             </div>
         );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        newSession: state.firestore.ordered.newSession
     }
 }
 
@@ -24,4 +36,9 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(UserPanel)
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    firestoreConnect([
+        {collection: 'newSession'}
+    ])
+    )(UserPanel)
