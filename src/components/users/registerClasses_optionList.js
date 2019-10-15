9ef0ced592f6d4d096b2bc5b375ce23d218dabce
Kuanisinajar <superbenben@livemail.tw>
2019-10-15 10:01:06 +0800

    Added basic logic

--- src/components/users/registerClasses_optionList.js
+++ src/components/users/registerClasses_optionList.js
@@ -7,11 +7,36 @@ class OptionList extends Component {
             return timestamp.toDate()
         });
         
+        const sortClasess = () => {
+            const sorted = [];
+            if (classes) {
+                for (let i = 0; i < 7; i ++) {
+                    const byDay = classes.filter((date) => {
+                        return date.getDay() === i
+                    })
+                    if (byDay.length) {
+                        sorted[i] = byDay
+                    } else {
+                        sorted[i] = null;
+                    }
+                }
+            }
+            return sorted;
+        }
+        
+        const sortedClasses = sortClasess();
         
 
         return (
             <div>
-                很多東西
+                {
+                    sortedClasses.map((dayArr, i) => {
+                        if (dayArr) {
+                            return <div key={i}>{`星期${i}有${dayArr.length}堂課`}</div>
+                        }
+                        
+                    })
+                }
             </div>
         )
     }
