6a8bec3a508d4fb6d99f711f60f5f2888003ec04
Kuanisinajar <superbenben@livemail.tw>
2019-10-15 11:51:14 +0800

    Added handleChange funciton in registerClasses

--- src/components/users/registerClasses.js
+++ src/components/users/registerClasses.js
@@ -7,21 +7,46 @@ import { firestoreConnect } from 'react-redux-firebase';
 import OptionList from './registerClasses_optionList';
 
 class RegisterClasses extends Component {
+
+    state = {
+        selected: []
+    }
+
+    handleChange = (e) => {
+        let selected = this.state.selected;
+        const inputValue = e.target.value;
+        if (selected.indexOf(inputValue) > -1) {
+            selected = selected.filter((item) => {
+                return item !== inputValue
+            })
+        } else {
+            selected.push(inputValue)
+        }
+        this.setState({
+            selected: selected
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submitted')
+    }
+
     render() {
         const session = this.props.session ? this.props.session[0].sortedByCourse : null;
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 {
                     session && session.map((item, i) => {
                         return (
                             <div key={i}>
-                                <p>{item.name}</p>
-                                <p>{`共${item.length}堂`}</p>
-                            </div>
+                                <input type="checkbox" name={item.name} value={item.name} onChange={this.handleChange} /> {`${item.name} 共${item.length}堂`}
+                            </div>  
                         )
                     })
                 }
-            </div>
+                <button>確認</button>
+            </form>
         )
     }
 }
