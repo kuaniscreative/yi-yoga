5e5360c7f79b936a5f0a2b309f6b2173608ecf1c
Kuanisinajar <superbenben@livemail.tw>
2019-10-17 10:02:02 +0800

    Changed registerToCourse in userAction

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -3,12 +3,23 @@ export const registerToCourse = (course, userId) => {
 
         const firestore = getFirestore();
 
-        firestore.collection('user').doc(userId).update({
-            registeredCourse: course
-        }).then(() => {
-            alert('報名成功');
-            document.location.href = '/';
-            dispatch({type: 'REGISTERED_TO_COURSE', course});
+        firestore.collection('user').doc(userId).get().then((res) => {
+            const userData = res.data();
+            const allClasses = userData.allClasses || []
+            // destruct courses and reconstruct to a new array of all classes
+            let newClasses = [];
+            course.forEach((item) => {
+                newClasses = newClasses.concat(item.classes);
+            })
+
+            firestore.collection('user').doc(userId).update({
+                allClasses: [...allClasses, ...newClasses],
+                registeredCourse: course
+            }).then(() => {
+                // alert('報名成功');
+                // document.location.href = '/';
+                dispatch({type: 'REGISTERED_TO_COURSE', course});
+            })
         })
     }
 }
