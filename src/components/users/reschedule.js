7812fad4afb41090a574e5a727a7701ad2f07b46
Kuanisinajar <superbenben@livemail.tw>
2019-10-21 13:06:45 +0800

    [Add] function | filter matched classes in reschedule process
    concept: student must reschedule within two month
    solution: filter out reschedulable class on click event

--- src/components/users/reschedule.js
+++ src/components/users/reschedule.js
@@ -1,49 +1,83 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 class Reschedule extends Component {
 
-    requestTimeTable = (classStamp) => {
-        const split = classStamp.split('/');
-        const yyyy = split[0];
-        const mm = parseInt(split[1], 10);
-        console.log(`請求${yyyy}.${mm} - ${(mm + 1) % 12}的課表`)
+    state = {
+        timeTable: []
     }
+
+    classFilter = classStamp => {
+        const split = classStamp.split("/");
+        const yyyy = split[0];
+        const mm = parseInt(split[1], 10) - 1;
+        const selectedDate = new Date(yyyy, mm);
+        const nextMonth = new Date(yyyy, mm + 1);
+
+        return this.props.classProfile.filter(profile => {
+            const month = profile.classDate.toDate().getMonth();
+            const year = profile.classDate.toDate().getFullYear();
+            return (
+                (month === selectedDate.getMonth() &&
+                    year === selectedDate.getFullYear()) ||
+                (month === nextMonth.getMonth() &&
+                    year === nextMonth.getFullYear())
+            );
+        });
+    };
+
+    requestTimeTable = classStamp => {
+        this.setState({
+            ...this.state,
+            timeTable: this.classFilter(classStamp)
+        })
+    };
 
     render() {
         return (
-            <div id='reschedule'>
-               補課安排
-               { this.props.reschedulable && this.props.reschedulable.map((classSingle, i) => {
-                    return (
-                        <div key={i}>
-                            {classSingle}
-                            <button onClick={() => {this.requestTimeTable(classSingle)}}>補課</button>
-                        </div>
-                    )
-               })}  
-               <Link to='/'>取消</Link>               
+            <div id="reschedule">
+                補課安排
+                {this.props.reschedulable &&
+                    this.props.reschedulable.map((classSingle, i) => {
+                        return (
+                            <div key={i}>
+                                {classSingle}
+                                <button
+                                    onClick={() => {
+                                        this.requestTimeTable(classSingle);
+                                    }}
+                                >
+                                    補課
+                                </button>
+                            </div>
+                        );
+                    })}
+                <Link to="/">取消</Link>
             </div>
-        )
-    } 
-}
-
-const mapStateToProps = (state) => {
-    const uid = state.firebase.auth.isempty ? undefined : state.firebase.auth.uid;
-    const userData = uid && state.firestore.ordered.user ? state.firestore.ordered.user.find((user) => {
-        return user.id === uid
-    }) : {};
-    return {
-        reschedulable: userData.reschedulable
+        );
     }
 }
 
+const mapStateToProps = state => {
+    const uid = state.firebase.auth.isempty
+        ? undefined
+        : state.firebase.auth.uid;
+    const userData =
+        uid && state.firestore.ordered.user
+            ? state.firestore.ordered.user.find(user => {
+                  return user.id === uid;
+              })
+            : {};
+    return {
+        reschedulable: userData.reschedulable,
+        classProfile: state.firestore.ordered.classProfile
+    };
+};
+
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([
-        {collection: 'user'}, {collection:'classProfile'}
-    ])
-)(Reschedule)
+    firestoreConnect([{ collection: "user" }, { collection: "classProfile" }])
+)(Reschedule);
