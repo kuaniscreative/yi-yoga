aca2bc3057d2e6e7e3f3ebfc261834319affe034
Kuanisinajar <superbenben@livemail.tw>
2019-10-26 14:26:29 +0800

    [Add] style | mainContentWrapper
    This class is for wrapping the main contents

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import { BrowserRouter, Route } from "react-router-dom";
 // Components
 import Main from "./components/main";
 import LogIn from "./components/users/logIn";
-import Register from './components/users/register';
-import Reschedule from './components/users/reschedule';
+import Register from "./components/users/register";
+import Reschedule from "./components/users/reschedule";
 import LeaveApplication from "./components/users/leaveApplication";
-import RegisterClasses from './components/users/registerClasses';
-import Admin from './components/admin/admin';
-
+import RegisterClasses from "./components/users/registerClasses";
+import Admin from "./components/admin/admin";
 
 class App extends Component {
     state = {
@@ -19,17 +18,21 @@ class App extends Component {
         return (
             <div className="App">
                 <BrowserRouter>
-                    <Route exact path="/" component={Main} />
-                    <Route path="/log-in" component={LogIn} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/reschedule" component={Reschedule} />
-                    <Route path="/register-classes" component={RegisterClasses} />
-                    <Route
-                        path="/leave-application"
-                        component={LeaveApplication}
-                    />
-                    
-                    <Route path='/admin' component={Admin} />
+                    <div className='mainContentWrapper'>
+                        <Route exact path="/" component={Main} />
+                        <Route path="/log-in" component={LogIn} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/reschedule" component={Reschedule} />
+                        <Route
+                            path="/register-classes"
+                            component={RegisterClasses}
+                        />
+                        <Route
+                            path="/leave-application"
+                            component={LeaveApplication}
+                        />
+                        <Route path="/admin" component={Admin} />
+                    </div>
                 </BrowserRouter>
             </div>
         );
