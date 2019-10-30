3073b4c4f4502732f52d2503c7b094531e55af5f
Kuanisinajar <superbenben@livemail.tw>
2019-10-30 17:20:07 +0800

    [Add] 全案 ｜ 功能 ｜ 點擊首頁按鈕時清空success message
    1. 當使用者點擊請假、報名、補課按鈕時，dispatch
    2. dispatch會把success message清掉

--- src/components/users/userPanel.js
+++ src/components/users/userPanel.js
@@ -8,16 +8,23 @@ import { firestoreConnect } from 'react-redux-firebase';
 import { signOut } from '../../actions/authActions';
 
 class UserPanel extends Component {
+    
+    clearSuccessMessage = (target = '') => {
+        const output = `CLEAR_SUCCESS_MESSAGE_${target}`
+        this.props.clearSuccessMessage(output);
+    }
+
+    registerClassProcessor = () => {
+        return this.props.newSession ? <Link to='/register-classes' onClick={() => {this.clearSuccessMessage('NEWSESSION')}}>報名</Link> : null;
+    }
+
     render() {
-
-        const registerClassProcessor = this.props.newSession ? <Link to='/register-classes'>報名</Link> : null;
-
         return (
             <div id='userPanel'>
                 <div className='welcomeMessage'>嗨，小明</div>
-                <Link to="/leave-application">請假</Link>
-                <Link to="/reschedule">補課</Link>
-                { registerClassProcessor }
+                <Link to="/leave-application" onClick={() => {this.clearSuccessMessage('LEAVE')}}>請假</Link>
+                <Link to="/reschedule" onClick={() => {this.clearSuccessMessage('RESCHEDULE')}}>補課</Link>
+                { this.registerClassProcessor() }
                 <button onClick={this.props.signOut}>登出</button>
             </div>
         );
@@ -32,7 +39,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        signOut: () => { dispatch(signOut()) }
+        signOut: () => { dispatch(signOut()) },
+        clearSuccessMessage: (dispatchType) => {
+            dispatch({type: dispatchType})
+        }
     }
 }
 
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,28 +1,42 @@
-const initState = {
-
-}
+const initState = {};
 
 const userReducer = (state = initState, action) => {
-    switch (action.type) {  
-        case 'REGISTERED_TO_COURSE':
-            console.log('REGISTERED_TO_COURSE');
+    switch (action.type) {
+        case "REGISTERED_TO_COURSE":
+            console.log("REGISTERED_TO_COURSE");
             return {
                 ...state,
                 registerClassSuccess: true
-            }
-        case 'LEAVE_APPLICATION_SUCCESS':
+            };
+        case "LEAVE_APPLICATION_SUCCESS":
             return {
                 ...state,
                 leaveApplicationSuccess: true
-            }
-        case 'CLEAR_SUCCESS_MESSAGE':
+            };
+        case "CLEAR_SUCCESS_MESSAGE_LEAVE":
             return {
                 ...state,
                 leaveApplicationSuccess: false
-            }
+            };
+        case "RESCHEDULE_ADD_SUCCESS":
+            return {
+                ...state,
+                reacheduleAddSuccess: true
+            };
+        case "RESCHEDULE_PENDING_SUCCESS":
+            return {
+                ...state,
+                reachedulePendingSuccess: true
+            };
+        case "CLEAR_SUCCESS_MESSAGE_RESCHEDULE":
+            return {
+                ...state,
+                reacheduleAddSuccess: false,
+                reachedulePendingSuccess: false
+            };
         default:
-            return state
+            return state;
     }
-}
+};
 
-export default userReducer
+export default userReducer;
