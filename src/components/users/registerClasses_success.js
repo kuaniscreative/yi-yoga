4f0d230f456c32e0df93c2bce2ec115806031d9c
Kuanisinajar <superbenben@livemail.tw>
2019-11-12 14:36:50 +0800

    [Add] 學生主頁 ｜樣式 ｜ 按鈕區域

--- src/components/users/registerClasses_success.js
+++ src/components/users/registerClasses_success.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 class RegisterClassSuccess extends Component {
     render() {
         return (
             <div>
-                成功啦！！
+                <p className="resultMessage_title">報名成功</p>
+                <div className="nextStepButtonsArea">
+                    <Link to="/" className="outlineButton">
+                        回首頁
+                    </Link>
+                </div>
             </div>
-        )
+        );
     }
 }
 
-export default RegisterClassSuccess
+export default RegisterClassSuccess;
--- src/components/users/userPanel.js
+++ src/components/users/userPanel.js
@@ -1,55 +1,86 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 // actions
-import { signOut } from '../../actions/authActions';
+import { signOut } from "../../actions/authActions";
 
 class UserPanel extends Component {
-    
-    clearSuccessMessage = (target = '') => {
-        const output = `CLEAR_SUCCESS_MESSAGE_${target}`
+    clearSuccessMessage = (target = "") => {
+        const output = `CLEAR_SUCCESS_MESSAGE_${target}`;
         console.log(output);
         this.props.clearSuccessMessage(output);
-    }
+    };
 
     registerClassProcessor = () => {
-        return this.props.newSession ? <Link to='/register-classes' onClick={() => {this.clearSuccessMessage('NEWSESSION')}}>報名</Link> : null;
-    }
+        return this.props.newSession ? (
+            <Link
+                to="/register-classes"
+                className="outlineButton"
+                onClick={() => {
+                    this.clearSuccessMessage("NEWSESSION");
+                }}
+            >
+                報名
+            </Link>
+        ) : null;
+    };
 
     render() {
         return (
-            <div id='userPanel'>
-                <div className='welcomeMessage'>嗨，小明</div>
-                <Link to="/leave-application" onClick={() => {this.clearSuccessMessage('LEAVE')}}>請假</Link>
-                <Link to="/reschedule" onClick={() => {this.clearSuccessMessage('RESCHEDULE')}}>補課</Link>
-                { this.registerClassProcessor() }
+            <div id="userPanel">
+                <div className="welcomeMessage">嗨，小明</div>
+                <div className="userPanel_actions">
+                    <Link
+                        to="/leave-application"
+                        className="outlineButton"
+                        onClick={() => {
+                            this.clearSuccessMessage("LEAVE");
+                        }}
+                    >
+                        請假
+                    </Link>
+                    <Link
+                        to="/reschedule"
+                        className="outlineButton"
+                        onClick={() => {
+                            this.clearSuccessMessage("RESCHEDULE");
+                        }}
+                    >
+                        補課
+                    </Link>
+                    {this.registerClassProcessor()}
+                </div>
+
                 <button onClick={this.props.signOut}>登出</button>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         newSession: state.firestore.ordered.newSession
-    }
-}
+    };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
     return {
-        signOut: () => { dispatch(signOut()) },
-        clearSuccessMessage: (dispatchType) => {
-            dispatch({type: dispatchType})
+        signOut: () => {
+            dispatch(signOut());
+        },
+        clearSuccessMessage: dispatchType => {
+            dispatch({ type: dispatchType });
         }
-    }
-}
+    };
+};
 
 export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    firestoreConnect([
-        {collection: 'newSession'}
-    ])
-    )(UserPanel)
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    ),
+    firestoreConnect([{ collection: "newSession" }])
+)(UserPanel);
