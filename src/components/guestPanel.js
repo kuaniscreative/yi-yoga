37cfdf9096fe9b4d290f7dfd0cdc9249d41b42a5
Kuanisinajar <superbenben@livemail.tw>
2019-11-14 15:56:44 +0800

    [Add] 主頁 ｜ 架構 ｜ heroMessage
    1. 更改userPanel、guestPanel架構
    2. 更改heroMessage樣式

--- src/components/guestPanel.js
+++ src/components/guestPanel.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 const GuestPanel = () => {
     return (
         <div id="guestPanel">
-            <div className='heroMessage'>
-                <p>最強凍齡瑜伽</p>
-                <p>沒有芝伊</p>
+            <div className="heroMessage">
+                <div className="heroMessage_main">
+                    <p>最強凍齡瑜伽</p>
+                    <p>沒有芝伊</p>
+                </div>
+                <div className="heroMessage_sub">
+                    <p>的最強課程管理系統</p>
+                </div>
             </div>
             <Link to="/log-in" className="outlineButton">
                 登入
--- src/components/users/userPanel.js
+++ src/components/users/userPanel.js
@@ -1,104 +1,20 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { compose } from "redux";
-import { firestoreConnect } from "react-redux-firebase";
+import React from "react";
 
-// components
-import StepIndicator from "../stepIndicator";
-
-class UserPanel extends Component {
-    clearSuccessMessage = (target = "") => {
-        const output = `CLEAR_SUCCESS_MESSAGE_${target}`;
-        this.props.clearSuccessMessage(output);
-    };
-
-    registerClassProcessor = () => {
-        return this.props.newSession ? (
-            <Link
-                to="/register-classes"
-                className="rectButton"
-                onClick={() => {
-                    this.clearSuccessMessage("NEWSESSION");
-                }}
-            >
-                <p className="rectButton_text">報名</p>
-            </Link>
-        ) : null;
-    };
-
-    render() {
-        const nickName = this.props.userData ? this.props.userData.nickName : null;
-        return (
-            <div id="userPanel">
-                <StepIndicator indicator={`嗨，${nickName}`} />
-                <div className="userPanel_infos"></div>
-                <div className="userPanel_actions">
-                    <Link
-                        to="/leave-application"
-                        className="rectButton"
-                        onClick={() => {
-                            this.clearSuccessMessage("LEAVE");
-                        }}
-                    >
-                        <p className="rectButton_text">請假</p>
-                    </Link>
-
-                    <Link
-                        to="/reschedule"
-                        className="rectButton"
-                        onClick={() => {
-                            this.clearSuccessMessage("RESCHEDULE");
-                        }}
-                    >
-                        <p className="rectButton_text">補課</p>
-                    </Link>
-                    <Link
-                        to={this.props.newSession ? "/register-classes" : '/'}
-                        className={this.props.newSession ? "rectButton" : "rectButton disableSelect"}
-                        onClick={() => {
-                            this.clearSuccessMessage("NEWSESSION");
-                        }}
-                    >
-                        <p className="rectButton_text">報名</p>
-                    </Link>
-                    <Link
-                        to="/info"
-                        className="rectButton"
-                    >
-                        <p className="rectButton_text">使用者及課堂資訊</p>
-                    </Link>
+const UserPanel = () => {
+    return (
+        <div id="userPanel">
+            <div className="heroMessage">
+                <div className="heroMessage_main">
+                    <p>親愛的</p>
+                    <p>一起運動吧</p>
+                </div>
+                <div className="heroMessage_sub">
+                    <p>麻煩的請假補課在這裡解決，</p>
+                    <p>讓芝伊輕鬆無負擔</p>
                 </div>
             </div>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    const uid = state.firebase.auth.uid;
-    const userData = state.firestore.ordered.user
-        ? state.firestore.ordered.user.find(user => {
-              return user.id === uid;
-          })
-        : null;
-    return {
-        userData: userData,
-        newSession: state.firestore.ordered.newSession
-    };
+        </div>
+    );
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        clearSuccessMessage: dispatchType => {
-            dispatch({ type: dispatchType });
-        }
-    };
-};
-
-export default compose(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    ),
-    firestoreConnect([{ collection: "newSession" }, { collection: 'user'}])
-)(UserPanel);
+export default UserPanel;
