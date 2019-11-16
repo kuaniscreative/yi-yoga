cf59059efe8bc9325786230dc86183a9066ca28b
Kuanisinajar <superbenben@livemail.tw>
2019-11-16 11:55:19 +0800

    [Add] 主頁 ｜ 架構 ｜ 註冊預設文字

--- src/components/users/register.js
+++ src/components/users/register.js
@@ -35,13 +35,13 @@ class Register extends Component {
                 <StepIndicator indicator='註冊帳號'/>
                 <form className='comfyForm innerContent' onSubmit={this.handleSubmit}>
                     <label>帳號</label>
-                    <input name='email' type="text" onChange={this.handleChange} />
+                    <input name='email' type="text" onChange={this.handleChange} placeholder="請輸入信箱"/>
                     <label>密碼</label>
-                    <input name='password' type="password" onChange={this.handleChange} />
+                    <input name='password' type="password" onChange={this.handleChange} placeholder="至少6位數之密碼"/>
                     <label>姓名</label>
-                    <input name='name' type="text" onChange={this.handleChange} />
+                    <input name='name' type="text" onChange={this.handleChange} placeholder="你的名字"/>
                     <label>暱稱</label>
-                    <input name='nickName' type="text" onChange={this.handleChange} />
+                    <input name='nickName' type="text" onChange={this.handleChange} placeholder="a.k.a"/>
                     <NextStepButtonsArea />
                 </form>
             </div>
