d05c7e79e9c06fd136a6a545e86c2aa9f79d04c7
Kuanisinajar <superbenben@livemail.tw>
2019-11-16 14:31:31 +0800

    [Add] 登出 ｜ 功能 ｜ 登出後重新導向

--- src/components/users/userAccount.js
+++ src/components/users/userAccount.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from 'react-router-dom';
 
 // actions
 import { signOut } from '../../actions/authActions';
 
-const UserAccount = ({signOut}) => {
+const UserAccount = ({signOut, uid}) => {
+    if (!uid) {
+        return <Redirect to='/' />
+    }
     return (
         <div>
             <button onClick={signOut}>登出</button>
         </div>
     );
 };
+
+const mapStateToProps = (state) => {
+    return {
+        uid: state.firebase.auth.uid
+    }
+}
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -21,6 +31,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(UserAccount);
