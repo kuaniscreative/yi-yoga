b5fe5ca024831eeb94cde7a869c7e50f33553e5b
Kuanisinajar <superbenben@livemail.tw>
2019-11-17 17:43:59 +0800

    [Add] 主頁 ｜ 架構 ｜ 補課規則

--- src/components/leaveRule.js
+++ src/components/leaveRule.js
@@ -12,6 +12,7 @@ const LeaveRule = () => {
                 <li>每月只能請假一次</li>
                 <li>需在課程開始前兩小時完成請假申請</li>
                 <li>請假之課程可於兩個月內申請補課</li>
+                <li>上課的人很多，補課空間很小，請盡量不要請假</li>
             </ul>
             <div className="nextStepButtonsArea--notFixed">
                 <Link to="/" className="outlineButton">
--- src/components/rescheduleRule.js
+++ src/components/rescheduleRule.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// components
+import StepIndicator from "./stepIndicator";
 
 const RescheduleRule = () => {
     return (
-        <div>
-            這裡將會顯示補課規則<br />
+        <div id='rescheduleRule' className='innerContent'>
+        <StepIndicator indicator="補課規則" />
+            <ul className="comfyList">
+                <li>請假之課程可於兩個月內申請補課</li>
+                <li>如果當前沒有任何空位，可安排候補</li>
+                <li>候補課堂有人請假時，會依照候補順序補上</li>
+                <li>候補上時你將會收到一封通知信</li>
+                <li>不可同時候補多堂課</li>
+                <li>如果沒有候補上且該課堂已開始，你將可以再次安排候補</li>
+            </ul>
+            <div className="nextStepButtonsArea--notFixed">
+                <Link to="/" className="outlineButton">
+                    回首頁
+                </Link>
+            </div>
             
         </div>
     )
