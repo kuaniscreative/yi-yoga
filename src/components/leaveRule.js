1e386198484689bb342b4859657c9d38789f9617
Kuanisinajar <superbenben@livemail.tw>
2019-11-17 17:34:47 +0800

    [Add] 主頁 ｜ 架構 ｜ 請假規則

--- src/components/leaveRule.js
+++ src/components/leaveRule.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
+
+// components
+import StepIndicator from "./stepIndicator";
 
 const LeaveRule = () => {
     return (
-        <div>
-            這裡將會顯示請假規則<br />
-            
+        <div id='leaveRule' className="innerContent">
+            <StepIndicator indicator="請假規則" />
+            <ul className="comfyList">
+                <li>每月只能請假一次</li>
+                <li>需在課程開始前兩小時完成請假申請</li>
+                <li>請假之課程可於兩個月內申請補課</li>
+            </ul>
+            <div className="nextStepButtonsArea--notFixed">
+                <Link to="/" className="outlineButton">
+                    回首頁
+                </Link>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default LeaveRule;
