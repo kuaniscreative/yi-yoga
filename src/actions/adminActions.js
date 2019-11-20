68e4c5cdd2d92a39a059f138a83ad77cc7dc4ee2
Kuanisinajar <superbenben@livemail.tw>
2019-11-20 17:29:51 +0800

    [Modify] admin ｜ 課程開放
    1. 修改紀錄結構
    2. registerSession fire時，會新增session、course、classProfile等資料

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -1,34 +1,66 @@
-export const registerSession = classes => {
+export const registerSession = sessionInfo => {
     return (dispatch, getState, { getFirestore, getFirebase }) => {
         const firestore = getFirestore();
 
         firestore
-            .collection("newSession")
-            .doc("KZ6kC4qlTuG3Y4CG1uw6")
-            .set(classes)
+            .collection("session")
+            .add({
+                name: sessionInfo.name,
+                classes: sessionInfo.classes,
+                open: true
+            })
+            .then((res) => {
+                const classes = sessionInfo.classes;
+                const sessionId = res.id;
+                
+                function addCouseProfile() {
+                    return firestore.collection('regularCourse').get().then((snap) => {
+                        const data = snap.docs.map((doc) => {
+                            return doc.data();
+                        });
+                        const promiseTasks = []
+    
+                        data.forEach((regularCourse) => {
+                            const matchClasses = classes.filter((classDate) => {
+                                const classDay = classDate.getDay();
+                                const classHour = classDate.getHours();
+                                const regularCourseDay = regularCourse.reference.toDate().getDay();
+                                const regularCourseHour = regularCourse.reference.toDate().getHours();
+                                return classDay === regularCourseDay && classHour === regularCourseHour
+                            }) 
+                            const task = firestore.collection('course').add({
+                                session: sessionId,
+                                classes: matchClasses,
+                                name: regularCourse.name,
+                            })
+                            promiseTasks.push(task);
+                        })
+                        
+                        return Promise.all(promiseTasks)
+                    })
+                }
+                function addClassProfile() {
+                    const promiseTasks = [];
+                    sessionInfo.classes.forEach((classDate) => {
+                        const task = firestore.collection("classProfile").add({
+                            classDate: classDate,
+                            absence: [],
+                            pendingStudents: [],
+                            rescheduleStudents: [],
+                            students: []
+                        });
+                        promiseTasks.push(task);
+                    })
+                    return Promise.all(promiseTasks);
+                }
+
+                const promiseTasks = [addCouseProfile(), addClassProfile()]
+                return Promise.all(promiseTasks);
+            })
             .then(() => {
                 alert("新的課程現在可以報名囉！");
-                dispatch({ type: "ADDED_NEW_SESSION", classes });
+                dispatch({ type: "ADDED_NEW_SESSION"});
             })
-            .catch(err => {
-                dispatch({ type: "ERROR", err });
-            });
     };
 };
 
-export const addClassProfile = (classes) => {
-    return (dispatch, getState, { getFirestore, getFirebase }) => {
-        const firestore = getFirestore();
-
-        for (let classSingle of classes) {
-          firestore.collection('classProfile').add({
-            classDate: classSingle,
-            absence: [],
-            pendingStudents: [],
-            rescheduleStudents: [],
-            students:[]
-          })
-        }
-        
-    };
-};
--- src/components/admin/newSession.js
+++ src/components/admin/newSession.js
@@ -1,115 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
 
-// components 
-import NewSessionForm from './newSession_form';
-import NewSessionPreview from './newSession_preview';
+// components
+import NewSessionForm from "./newSession_form";
+import NewSessionPreview from "./newSession_preview";
 
 // actions
-import { registerSession, addClassProfile } from '../../actions/adminActions';
-
-// functions
-import { uidGenerator } from '../../functions/uid';
+import { registerSession } from "../../actions/adminActions";
 
 class NewSession extends Component {
-
-
     state = {
         period: [],
         classes: [],
-        regularSession: [
-            {day: 1, hour: 19, minute: 20, name: '星期一 19:20 - 20:20'},
-            {day: 1, hour: 20, minute: 30, name: '星期一 20:30 - 21:30'},
-            {day: 2, hour: 19, minute: 20, name: '星期二 19:10 - 20:10'},
-            {day: 2, hour: 20, minute: 30, name: '星期二 20:30 - 21:30'},
-            {day: 4, hour: 18, minute: 50, name: '星期四 18:50 - 19:50'},
-            {day: 5, hour: 19, minute: 30, name: '星期五 19:30 - 20:30'}
-        ],
         periodInputIsValid: true
-    }
+    };
 
     getSession = (start, end) => {
         const startDate = new Date(start.year, start.month - 1, 1);
         const endDate = new Date(end.year, end.month, 0);
-        const regularSession = this.state.regularSession;
-        const targets = []
-        const currentSessions = this.props.sessions; 
-        const checkValidPeriod = () => {
-            // for (let session of currentSessions) {
-            //     for (let period of session.period) {
-            //         if (period.year === start.year && period.month === start.month) {
-            //             return false
-            //         } 
-            //         if (period.year === end.year && period.month === end.month) {
-            //             return false
-            //         }
-            //     }
-            // }
-            return true
-        }
+        const regularCourse = this.props.regularCourse;
+        const targets = [];
 
-        if (checkValidPeriod()) {
-            // get the classes and push them to targets
-            while(startDate.valueOf() !== endDate.valueOf()) {
-                switch (startDate.getDay()) {
-                    case 1: 
-                        (() => {
-                            let t = regularSession.filter((item) => {
-                                return item.day === 1
-                            });
-                            t.forEach((item) => {
-                                const d = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), item.hour, item.minute);
-                                d.id = uidGenerator();
-                                targets.push(d);
-                            });
-                        })();
-                        break
-                    case 2: 
-                        (() => {
-                            let t = regularSession.filter((item) => {
-                                return item.day === 2
-                            });
-                            t.forEach((item) => {
-                                const d = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), item.hour, item.minute);
-                                d.id = uidGenerator();
-                                targets.push(d);
-                            });
-                        })();
-                        break
-                    case 4: 
-                        (() => {
-                            let t = regularSession.filter((item) => {
-                                return item.day === 4
-                            });
-                            t.forEach((item) => {
-                                const d = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), item.hour, item.minute);
-                                d.id = uidGenerator();
-                                targets.push(d);
-                            });
-                        })();
-                        break
-                    case 5: 
-                        (() => {
-                            let t = regularSession.filter((item) => {
-                                return item.day === 5
-                            });
-                            t.forEach((item) => {
-                                const d = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), item.hour, item.minute);
-                                d.id = uidGenerator();
-                                targets.push(d);
-                            });
-                        })();
-                        break
-                }
-                
-                startDate.setDate(startDate.getDate() + 1);
-                
+        // get the classes and push them to targets
+        while (startDate.valueOf() !== endDate.valueOf()) {
+            const day = startDate.getDay();
+            const match = regularCourse.find((course) => {
+                return course.dayNum === day
+            })
+
+            if (match) {
+                const d = new Date(
+                    startDate.getFullYear(),
+                    startDate.getMonth(),
+                    startDate.getDate(),
+                    match.reference.toDate().getHours(),
+                    match.reference.toDate().getMinutes()
+                );
+                targets.push(d);
             }
-        } 
-        return targets
-    }
+
+            startDate.setDate(startDate.getDate() + 1);
+        }
+        return targets;
+    };
 
     setSessionPeriod = (start, end) => {
         const classes = this.getSession(start, end);
@@ -117,97 +52,72 @@ class NewSession extends Component {
             this.setState({
                 ...this.state,
                 periodInputIsValid: false
-            })
+            });
         } else {
             this.setState({
                 ...this.state,
                 period: [start, end],
                 classes: classes
-            })
+            });
         }
-    }
+    };
 
-    deleteClassWhenPreview = (id) => {
-        const classes = this.state.classes.filter((classSingle) => {
-            return classSingle.id !== id
-        })
+    deleteClassWhenPreview = id => {
+        const classes = this.state.classes.filter(classSingle => {
+            return classSingle.id !== id;
+        });
         this.setState({
             ...this.state,
             classes: classes
-        })
-    }
+        });
+    };
 
-    sortClassesByCourse = (classesArr) => {
-        const regularSession = this.state.regularSession;
-        const result = [];
-        regularSession.forEach((regular) => {
-            const name = regular.name;
-            const match = [];
-            classesArr.forEach((classSingle) => {
-                if (regular.day === classSingle.getDay() && regular.hour === classSingle.getHours()) {
-                    match.push(classSingle);
-                }
-            }) 
-            result.push({
-                name: name,
-                classes: match,
-                length: match.length
-            })
-        })
-        return result
-    }
-    
     addSession = () => {
-        
+        const period = this.state.period;
+        const name = `${period[0].year}年 ${period[0].month}月 - ${period[1].month}月`;
         const sessionInfo = {
-            period: this.state.period,
-            classes: this.state.classes,
-            all: this.state.classes,
-            sortedByCourse: this.sortClassesByCourse(this.state.classes)
-        }
-        this.props.registerSession(sessionInfo)
-        this.props.addClassProfile(this.state.classes);
-    }
+            name: name,
+            classes: this.state.classes
+        };
+        this.props.registerSession(sessionInfo);
+    };
 
     render() {
-        const newSessionProcesser = this.state.classes.length ? 
-            <NewSessionPreview classes={this.state.classes} deleteClassWhenPreview={this.deleteClassWhenPreview} addSession={this.addSession} /> :
-            <NewSessionForm setSessionPeriod={this.setSessionPeriod} validPeriod={this.state.periodInputIsValid} />;
-        
-        return (
-            <div>
-                {
-                    newSessionProcesser
-                }              
-            </div>
-        )
+        const newSessionProcesser = this.state.classes.length ? (
+            <NewSessionPreview
+                classes={this.state.classes}
+                deleteClassWhenPreview={this.deleteClassWhenPreview}
+                addSession={this.addSession}
+            />
+        ) : (
+            <NewSessionForm
+                setSessionPeriod={this.setSessionPeriod}
+                validPeriod={this.state.periodInputIsValid}
+            />
+        );
+
+        return <div>{newSessionProcesser}</div>;
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    console.log(state);
     return {
-        sessions: state.firestore.ordered.newSession,
+        regularCourse: state.firestore.ordered.regularCourse,
         newSessionIsAdded: state.admin.newSessionIsAdded,
-        classProfile: state.firestore.ordered.classProfile
+    };
+};
 
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
     return {
-        registerSession: (session) => {
-            dispatch(registerSession(session))
-        },
-        addClassProfile: (classes) => {
-            dispatch(addClassProfile(classes))
+        registerSession: sessionInfo => {
+            dispatch(registerSession(sessionInfo));
         }
-    }
-}
+    };
+};
 
 export default compose(
     connect(mapStateToProps, mapDispatchToProps),
     firestoreConnect([
-        {collection: 'newSession'}, {collection: 'classProfile'}
+        { collection: "regularCourse" }
     ])
-    )(NewSession)
+)(NewSession);
