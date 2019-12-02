e374f9ddc8b1ede23776a572eb531b812f7b5877
Kuanisinajar <superbenben@livemail.tw>
2019-12-02 13:36:53 +0800

    [Modify] 補課 ｜ 樣式 ｜ success樣式

--- src/components/users/reschedule_success.js
+++ src/components/users/reschedule_success.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+// components
+import StepIndicator from "../stepIndicator";
 
 const RescheduleSuccess = ({ status }) => {
     const pendingOutput = () => {
@@ -22,28 +25,31 @@ const RescheduleSuccess = ({ status }) => {
         return (
             <div className="innerContent">
                 <p className="resultMessage_title">補課完成</p>
-                <p className="resultMessage_text">
-                    親愛的我們到時見
-                </p>
+                <p className="resultMessage_text">親愛的我們到時見</p>
                 <div className="nextStepButtonsArea--notFixed">
                     <Link to="/" className="outlineButton">
                         回首頁
                     </Link>
                 </div>
             </div>
-        )
-    }
+        );
+    };
     const output = () => {
-        switch(status) {
-            case 'success': 
-            return successOutput()
-            case 'pending':
-                return pendingOutput()
-            default: 
-                break
+        switch (status) {
+            case "success":
+                return successOutput();
+            case "pending":
+                return pendingOutput();
+            default:
+                break;
         }
-    }
-    return <div>{output()}</div>;
+    };
+    return (
+        <div id='reschedule_success'>
+            <StepIndicator indicator="補課結果" />
+            {output()}
+        </div>
+    );
 };
 
 export default RescheduleSuccess;
