b34c561e823ad0f8896a96f5f0dd614e6245ac36
Kuanisinajar <superbenben@livemail.tw>
2019-12-04 15:18:26 +0800

    [Modify] 更改請假成功頁面layout

--- src/components/users/leaveApplication_success.js
+++ src/components/users/leaveApplication_success.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 
+// components
+import StepIndicator from '../stepIndicator';
+
 const LeaveApplicationSuccess = ({clearSuccessMessage}) => {
     return (
-        <div className="innerContent">
+        <div className="layout_contentBlock">
+            <StepIndicator indicator='請假結果'/>
             <p className="resultMessage_title">請假成功</p>
             <p className="resultMessage_action">
-                <Link to='/reschedule'>現在去補課 &rarr;</Link>
+                <Link to='/reschedule'>現在去安排補課 &rarr;</Link>
             </p>
             <div className="nextStepButtonsArea--notFixed">
                 <Link to="/" className="outlineButton" onClick={() => {clearSuccessMessage('RESCHEDULE')}}>
