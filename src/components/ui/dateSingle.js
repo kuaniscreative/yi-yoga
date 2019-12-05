189686d67962a9acecf95752d0e5dfc2eeb2cd4c
Kuanisinajar <superbenben@livemail.tw>
2019-12-05 11:40:12 +0800

    [Add] 課程狀態 ｜ 架構 ｜ 新增所有課程component

--- src/components/ui/dateSingle.js
+++ src/components/ui/dateSingle.js
@@ -10,12 +10,12 @@ const DateSingle = ({ date }) => {
     const day = date.getDay();
     const dayOutput = `週${day.toLocaleString("zh-u-nu-hanidec")}`;
     return (
-        <div className="dateHero checkboxContainer_message">
-            <div className="dateHero">
-                <span name="date">{`${dd}`}</span>
-                <span name="monthYear">{`${mm + 1}月 ${yyyy}`}</span>
-                <span name="seperator"> | </span>
-                <span name="dayTime">{`${dayOutput} ${startAt}`}</span>
+        <div className="dateSingle">
+            <div className="dateSingle_date">{`${yyyy}年${mm +
+                1}月${dd}日`}</div>
+            <div className="dateSingle_other">
+                <span>{dayOutput}</span>
+                <span>{startAt}</span>
             </div>
         </div>
     );
--- src/components/users/userStatus.js
+++ src/components/users/userStatus.js
@@ -7,12 +7,20 @@ import { firestoreConnect } from "react-redux-firebase";
 import StepIndicator from "../stepIndicator";
 import LeaveSingle from "./userStatus_leaveSingle";
 import RescheduleSingle from "./userStatus_rescheduleSingle";
-import PaymentSingle from './userStatus_paymentSingle';
+import PaymentSingle from "./userStatus_paymentSingle";
+import ItemBarWithAction from "../ui/itemBarWithAction";
+import ClassSingle from './userStatus_classSingle';
 
-const UserStatus = ({ uid, leaveRecord, classProfile, payments }) => {
+const UserStatus = ({
+    uid,
+    leaveRecord,
+    classProfile,
+    payments,
+    userClasses
+}) => {
     return (
         <div id="userStatus" className="innerContent">
-            <div className='layout_pageTitle'>
+            <div className="layout_pageTitle">
                 <div className="wrapper">
                     <h1>課程狀態</h1>
                 </div>
@@ -24,17 +32,15 @@ const UserStatus = ({ uid, leaveRecord, classProfile, payments }) => {
              */}
             <div className="layout_contentBlock">
                 <StepIndicator indicator="報名狀態" />
-                { payments && payments.map((payment, i) => {
-                    return <PaymentSingle infos={payment} key={i}/>
-                })}
-                {
-                    /** default*/
-                    payments && !payments.length ? (
-                        <p name="default">沒有任何報名資訊</p>
-                    ) : null
-                }
+                {payments &&
+                    payments.map((payment, i) => {
+                        return <PaymentSingle infos={payment} key={i} />;
+                    })}
+                {/** default*/
+                payments && !payments.length ? (
+                    <p name="default">沒有任何報名資訊</p>
+                ) : null}
             </div>
-
 
             {/**
              *
@@ -86,14 +92,14 @@ const UserStatus = ({ uid, leaveRecord, classProfile, payments }) => {
                             />
                         );
                     })}
-                {
-                    /** default*/
-                    leaveRecord && !leaveRecord.rescheduled.length && !leaveRecord.reschedulePending.length && !leaveRecord.reschedulable.length ? (
-                        <p name="default">沒有任何請假資訊</p>
-                    ) : null
-                }
+                {/** default*/
+                leaveRecord &&
+                !leaveRecord.rescheduled.length &&
+                !leaveRecord.reschedulePending.length &&
+                !leaveRecord.reschedulable.length ? (
+                    <p name="default">沒有任何請假資訊</p>
+                ) : null}
             </div>
-
 
             {/**
              *
@@ -131,12 +137,31 @@ const UserStatus = ({ uid, leaveRecord, classProfile, payments }) => {
                             />
                         );
                     })}
-                {
-                    /** default*/
-                    leaveRecord && !leaveRecord.rescheduled.length && !leaveRecord.reschedulePending.length ? (
-                        <p name="default">沒有任何補課資訊</p>
-                    ) : null
-                }
+                {/** default*/
+                leaveRecord &&
+                !leaveRecord.rescheduled.length &&
+                !leaveRecord.reschedulePending.length ? (
+                    <p name="default">沒有任何補課資訊</p>
+                ) : null}
+
+                {/**
+                 *
+                 *   所有課程
+                 *
+                 */}
+            </div>
+            <div className="layout_contentBlock">
+                <StepIndicator indicator="所有課程" />
+                <ul className="borderBottomList">
+                    {userClasses.map(classInfo => {
+                        
+                        return (
+                            <li>
+                                <ClassSingle classInfo={classInfo}/>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     );
@@ -144,19 +169,107 @@ const UserStatus = ({ uid, leaveRecord, classProfile, payments }) => {
 
 const mapStateToProps = state => {
     const uid = state.firebase.auth.uid;
+    const users = state.firestore.ordered.user;
+    const classProfile = state.firestore.ordered.classProfile;
     const leaveRecord = state.firestore.ordered.leaveRecord
         ? state.firestore.ordered.leaveRecord.find(record => {
               return record.id === uid;
           })
         : null;
-    const payments = state.firestore.ordered.paymentStatus && state.firestore.ordered.paymentStatus.filter((status) => {
-        return status.owner === uid
-    })
+    const payments =
+        state.firestore.ordered.paymentStatus &&
+        state.firestore.ordered.paymentStatus.filter(status => {
+            return status.owner === uid;
+        });
+
+    const userData =
+        users && uid
+            ? users.find(user => {
+                  return user.id === uid;
+              })
+            : null;
+
+    const processedData = () => {
+        const allClasses = userData ? userData.allClasses : null;
+        const leaveClasses = userData ? userData.leave : null;
+        const reschedulable = leaveRecord ? leaveRecord.reschedulable : null;
+        const rescheduled = leaveRecord ? leaveRecord.rescheduled : null;
+        const reschedulePending = leaveRecord
+            ? leaveRecord.reschedulePending
+            : null;
+
+        const classes = [];
+        allClasses &&
+            allClasses.forEach(classInfo => {
+                classes.push({
+                    date: classInfo.date.toDate()
+                });
+            });
+        reschedulable &&
+            reschedulable.forEach(date => {
+                const newInfo = {
+                    date: date.toDate(),
+                    label: "reschedulable"
+                };
+
+                classes.push(newInfo);
+            });
+        rescheduled &&
+            rescheduled.forEach(info => {
+                const reshceduleClassInfo =
+                    classProfile &&
+                    classProfile.find(profile => {
+                        return profile.id === info.rescheduleClassId;
+                    });
+                reshceduleClassInfo &&
+                    classes.push({
+                        date: reshceduleClassInfo.classDate.toDate(),
+                        label: "rescheduled"
+                    });
+
+                const existed = classes.find(classInfo => {
+                    return classInfo.date.seconds === info.leaveDate.seconds;
+                });
+                if (!existed) {
+                    classes.push({
+                        date: info.leaveDate.toDate(),
+                        label: "leave"
+                    });
+                }
+            });
+        reschedulePending &&
+            reschedulePending.forEach(info => {
+                const pendingClassInfo =
+                    classProfile &&
+                    classProfile.find(profile => {
+                        return profile.id === info.pendingClassId;
+                    });
+                pendingClassInfo &&
+                    classes.push({
+                        date: pendingClassInfo.classDate,
+                        label: "reschedulePending"
+                    });
+
+                const existed = classes.find(classInfo => {
+                    return classInfo.date.seconds === info.leaveDate.seconds;
+                });
+                if (!existed) {
+                    classes.push({
+                        date: info.leaveDate,
+                        label: "leave"
+                    });
+                }
+            });
+
+        return classes;
+    };
+
     return {
         uid: uid,
         leaveRecord: leaveRecord,
         classProfile: state.firestore.ordered.classProfile,
-        payments: payments
+        payments: payments,
+        userClasses: processedData()
     };
 };
 
@@ -166,6 +279,6 @@ export default compose(
         { collection: "user" },
         { collection: "leaveRecord" },
         { collection: "classProfile" },
-        { collection: 'paymentStatus' }
+        { collection: "paymentStatus" }
     ])
 )(UserStatus);
