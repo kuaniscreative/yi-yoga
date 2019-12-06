96ff75e1d11b38259b626d4c32769285e40827b0
Kuanisinajar <superbenben@livemail.tw>
2019-12-06 18:15:12 +0800

    [Modify] 更換補課DateSingle component
    1. dateHero改為dateSingle
    2. 新增iphoneSE樣式

--- src/components/users/reschedule_preview_LeaveClassesList.js
+++ src/components/users/reschedule_preview_LeaveClassesList.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+// conponents
+import DateSingle from '../ui/dateSingle';
+
 class LeaveClassesList extends Component {
 
     options = () => {
@@ -11,24 +14,15 @@ class LeaveClassesList extends Component {
                 const date = timestamp.toDate();
                 const yyyy = date.getFullYear();
                 const mm = date.getMonth();
-                const dd = date.getDate();
-                const hr = date.getHours();
-                const min = date.getMinutes();
-                const startAt = `${hr}:${min}`;
-                const day = date.getDay();
-                const dayOutput = `週${day.toLocaleString("zh-u-nu-hanidec")}`;
                 return (
                     <div
-                        className="shadowOption dateHero checkboxContainer_message"
+                        className="shadowOption  checkboxContainer_message"
                         key={i}
                         data-month={mm}
                         data-year={yyyy}
                         onClick={() => {this.props.selectLeaveClass(mm, yyyy, date)}}
                     >
-                        <span name="date">{`${dd}`}</span>
-                        <span name="monthYear">{`${mm + 1}月 ${yyyy}`}</span>
-                        <span name="seperator"> | </span>
-                        <span name="dayTime">{`${dayOutput} ${startAt}`}</span>
+                       <DateSingle date={date}/>
                     </div>
                 );
             })
