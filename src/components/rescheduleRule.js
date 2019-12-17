722c5ceddda4ce6243642d61c274f1431ad15c8b
Kuanisinajar <superbenben@livemail.tw>
2019-12-17 12:30:57 +0800

    [Add] 新增補課規則

--- src/components/rescheduleRule.js
+++ src/components/rescheduleRule.js
@@ -12,11 +12,12 @@ const RescheduleRule = () => {
             <div className="layout_contentBlock">
                 <ul className="comfyList">
                     <li>請假之課程可於兩個月內申請補課</li>
-                    <li>如果當前沒有任何空位，可安排候補</li>
-                    <li>候補課堂有人請假時，會依照候補順序補上</li>
+                    <li>如果課堂沒有空位，系統會安排候補</li>
+                    <li>候補課堂有人請假時，依照候補順序補上</li>
                     <li>候補上時你將會收到一封通知信</li>
+                    <li>如果課堂將在兩小時內開始，系統會停止自動安排補課，並寄信詢問你補課的意願</li>
+                    <li>如果沒有候補上且該課堂已開始，系統會退還補課機會</li>
                     <li>不可同時候補多堂課</li>
-                    <li>如果沒有候補上且該課堂已開始，你將可以再次安排候補</li>
                 </ul>
                 <div className="nextStepButtonsArea--notFixed">
                     <Link to="/" className="outlineButton">
