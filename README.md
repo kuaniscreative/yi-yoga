6a143ca74933390f14485a52efc54d8d9f8712d7
Kuanisinajar <superbenben@livemail.tw>
2020-02-11 17:00:41 +0800

    [Add] env switch

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 A class manage system for Yi Yoga.
 
-
-
 ## Getting Started
 
 - `$ npm install` to install packages
@@ -11,7 +9,12 @@ A class manage system for Yi Yoga.
 - `$ gulp` to listen to SCSS file changes
 - `$ npm run build` for a production build
 
+## Before Deployment
 
+- 記得切換 firebase environment。
+- `$ firebase use default` for **production**
+- `$ firebase use development` for **development**
+- 發佈時會自動更新 `NODE_ENV` ， `src/fbConfig.js` 會自動切換。（關於 `NODE_ENV` 的設定請參考 [create-react-app 文件](https://create-react-app.dev/docs/adding-custom-environment-variables/)）
 
 ## Project Information
 
@@ -27,5 +30,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 **Hosting**: [Firebase](https://firebase.google.com/)
 
+## MISC
 
-
+- 切換環境參考[此篇](https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial)
---
+++ src/fbConfig.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/functions';
+
+const prodConfig = {
+  apiKey: 'AIzaSyBn-U0qM2EaZOheotCkel7VO-QToNCOh4s',
+  authDomain: 'class-manage-80e60.firebaseapp.com',
+  databaseURL: 'https://class-manage-80e60.firebaseio.com',
+  projectId: 'class-manage-80e60',
+  storageBucket: 'class-manage-80e60.appspot.com',
+  messagingSenderId: '215301096460',
+  appId: '1:215301096460:web:a1be60462745aacc9d9a29',
+  measurementId: 'G-43MPXB80T8'
+};
+
+const devConfig = {
+  apiKey: 'AIzaSyCs1r4iDSHJ-xK6E3BFswzmb2zdZl00g6M',
+  authDomain: 'class-manage-dev.firebaseapp.com',
+  databaseURL: 'https://class-manage-dev.firebaseio.com',
+  projectId: 'class-manage-dev',
+  storageBucket: 'class-manage-dev.appspot.com',
+  messagingSenderId: '310170459856',
+  appId: '1:310170459856:web:1e75c9b40be43f13b0c8d5'
+};
+
+const config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
+// Initialize Firebase*
+
+firebase.initializeApp(config);
+console.log(config);
+console.log(process.env.NODE_ENV);
+
+export default firebase;
