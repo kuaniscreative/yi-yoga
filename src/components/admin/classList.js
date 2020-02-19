7825c7e61a32335b730197d8cedf386f2917fca1
Kuanisinajar <superbenben@livemail.tw>
2020-02-19 15:44:42 +0800

    [Add] Connect ClassList to regularCourseContext

--- src/components/admin/classList.js
+++ src/components/admin/classList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 
 // components
@@ -6,21 +6,33 @@ import TitleBlock from '../ui/titleBlock';
 import Block from '../ui/block';
 import ButtonGroup from '../ui/buttonGroup';
 
-class ClassList extends Component {
-  render() {
-    return (
-      <div>
-        <TitleBlock title="查看課表" />
-        <Block>
-          <ButtonGroup>
-            <button>第一行</button>
-            <button>第一行自不一樣</button>
-            <button>第一行</button>
-          </ButtonGroup>
-        </Block>
-      </div>
-    );
-  }
-}
+// contexts
+import { regularCourseContext } from '../contexts/regularCourseContext';
+
+const ClassList = () => {
+  const { regularCourse } = useContext(regularCourseContext);
+  const courseOptions = regularCourse.reduce((acc, cVal) => {
+    const day = cVal.day;
+    if (acc.indexOf(day) < 0) {
+      return [...acc, day];
+    }
+  }, []);
+
+  return (
+    <div>
+      <TitleBlock title="查看課表" />
+      <Block>
+        <ButtonGroup>
+          <button>所有課堂</button>
+          <button>未額滿</button>
+        </ButtonGroup>
+      </Block>
+      <Block>
+        <button className="outlineButton">一月</button>
+        <button className="outlineButton">二月</button>
+      </Block>
+    </div>
+  );
+};
 
 export default ClassList;
