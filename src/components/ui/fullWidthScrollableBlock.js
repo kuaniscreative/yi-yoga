329ee8606616491f6b79be592a8dbdf11a285aa4
Kuanisinajar <superbenben@livemail.tw>
2020-02-22 12:38:04 +0800

    [Modify] ClassList composition
    1. ClassList 僅在 mobile 時包裹並可以滑動檢視
    2. 新增 scrollHint

--- src/components/ui/fullWidthScrollableBlock.js
+++ src/components/ui/fullWidthScrollableBlock.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// components
+import { ReactComponent as Arrow } from '../../static/arrow.svg';
+
+// data
+import theme from '../../json/theme.json';
+
 const RowNoWrap = styled.div`
-  flex-wrap: nowrap;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding: 2rem 3rem;
+  padding-top: 1rem;
   overflow: scroll;
   &::-webkit-scrollbar {
     display: none;
+  }
+  @media (max-width: 576px) {
+    flex-wrap: nowrap;
+    padding-right: 0;
+    padding-left: 0;
+  }
+`;
+const ScrollHint = styled.div`
+  display: none;
+  font-size: 0.75rem;
+  font-weight: 500;
+  color: ${theme.colors.gray2};
+  vertical-align: middle;
+  user-select: none;
+
+  span {
+    padding-right: 4px;
+  }
+
+  @media (max-width: 576px) {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
   }
 `;
 
@@ -15,6 +43,16 @@ const FullWidthScrollableBlock = (props) => {
   const { children } = props;
   return (
     <div className="container-fluid">
+      <div className="row">
+        {children.length > 0 ? (
+          <div className="col-4 offset-8">
+            <ScrollHint>
+              <span>滑動檢視</span>
+              <Arrow stroke={theme.colors.gray2} width="18" height="18" />
+            </ScrollHint>
+          </div>
+        ) : null}
+      </div>
       <RowNoWrap className="row">{children}</RowNoWrap>
     </div>
   );
