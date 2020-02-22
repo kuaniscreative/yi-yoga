116a62e64ded08e93e0c2a559262197ca6062313
Kuanisinajar <superbenben@livemail.tw>
2020-02-22 18:09:03 +0800

    [Add] SessionItem showing payment status

--- src/components/admin/paymentStatus_sessionItem.js
+++ src/components/admin/paymentStatus_sessionItem.js
@@ -4,13 +4,30 @@ import styled from 'styled-components';
 const SessionTitle = styled.div`
   font-size: 1.5rem;
   letter-spacing: normal;
+  margin-bottom: 0.75em;
+`;
+const PaymentStatusButton = styled.button`
+  display: inline-block;
+  position: relative;
+  margin-right: 1.5rem;
 `;
 
 const SessionItem = (props) => {
   const { name, payments } = props;
+  const { finished, paid, pending } = payments;
+
   return (
     <div>
       <SessionTitle>{name}</SessionTitle>
+      <PaymentStatusButton>
+        未繳費：<u>{pending.length}</u>
+      </PaymentStatusButton>
+      <PaymentStatusButton>
+        待確認：<u>{paid.length}</u>
+      </PaymentStatusButton>
+      <PaymentStatusButton>
+        已完成：<u>{finished.length}</u>
+      </PaymentStatusButton>
     </div>
   );
 };
