509cb60f8d5d2d92c3d065eb602954c744e9f366
Kuanisinajar <superbenben@livemail.tw>
2020-02-24 16:18:02 +0800

    [Modify] StudentCard now using styled-component

--- src/components/admin/studentCard.js
+++ src/components/admin/studentCard.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// components
+import NameTag from '../ui/nameTag';
 
 //actions
 import { validateStudent } from '../../actions/adminActions';
+
+// data
+import theme from '../../json/theme.json';
+
+const Card = styled.div`
+  padding: 2rem 1.5rem;
+  margin-bottom: 2rem;
+  border: 1px solid ${theme.colors.gray2};
+  border-radius: 4px;
+`;
+const Infos = styled.div`
+  padding-bottom: 1.5rem;
+  border-bottom: 1px solid ${theme.colors.gray1};
+  margin-bottom: 1.5rem;
+`;
+
+const Email = styled.p`
+  padding-top: 1em;
+  font-size: 0.8rem;
+`;
+
+const Message = styled.div`
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+  font-size: 0.9rem;
+  color: ${theme.colors.gray4};
+`;
+
+const Action = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 3rem;
+`;
 
 const StudentCard = (props) => {
   const { name, nickName, email, message, id } = props.data;
@@ -13,23 +50,20 @@ const StudentCard = (props) => {
     }
   };
   return (
-    <div className="studentCard">
-      <div className="studentCard_info">
-        <h1 className="studentCard_name">
-          <span className="name">{name}</span>
-          <span className="nickName">{nickName}</span>
-        </h1>
-        <p className="studentCard_email">{email}</p>
-      </div>
-      <div className="studentCard_message">
+    <Card>
+      <Infos>
+        <NameTag name={name} nickName={nickName} />
+        <Email>{email}</Email>
+      </Infos>
+      <Message>
         <p>{message}</p>
-      </div>
-      <div className="studentCard_action">
+      </Message>
+      <Action>
         <button className="outlineButton" data-id={id} onClick={handleClick}>
           開放使用
         </button>
-      </div>
-    </div>
+      </Action>
+    </Card>
   );
 };
 
