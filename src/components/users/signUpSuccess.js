1878031719a15ace12b39ad0b6337b91e3a1049f
Kuanisinajar <superbenben@livemail.tw>
2020-02-25 15:39:13 +0800

    [Add] SignUpSuccess layout

--- src/components/users/signUpSuccess.js
+++ src/components/users/signUpSuccess.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 // components
-import Container from '../ui/container';
-import Row from '../ui/row';
+import TitleBlock from '../ui/titleBlock';
+import Block from '../ui/block';
+import ArrowIconLink from '../ui/arrowIconLink';
 
 const SignUpSuccess = () => {
   return (
     <div>
-      <div className="layout_pageTitle">
-        <div className="wrapper">
-          <h1>註冊成功</h1>
-        </div>
-      </div>
-      <div className="layout_contentBlock">
+      <TitleBlock title="註冊成功" />
+      <Block>
         <ul className="comfyList">
           <li>要等待芝伊確認過後才能開始報名</li>
           <li>確認後會寄 email 通知你</li>
         </ul>
-        <div className="container-fluid">
-          <div className="row py-5">
-            <div className="col-12">
-              <Link to="/" className="outlineButton">
-                確認
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+      </Block>
+      <Block>
+        <ArrowIconLink to="/">回首頁</ArrowIconLink>
+      </Block>
     </div>
   );
 };
