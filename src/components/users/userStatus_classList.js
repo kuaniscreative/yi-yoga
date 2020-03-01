eb26476f695a1b7b1622a57179473030743c88ac
Kuanisinajar <superbenben@livemail.tw>
2020-03-01 20:11:55 +0800

    [Add] userStatus all classes default

--- src/components/users/userStatus_classList.js
+++ src/components/users/userStatus_classList.js
@@ -2,21 +2,31 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 
 // components
-import List, { ListItem } from '../ui/bottomLineList';
-import DateSingle from '../ui/dateSingle';
 import UserClassList from './userStatus_userClassList';
 
 // contexts
 import { userStatusContext } from '../contexts/userStatusContext';
 
-// functions
-import keyGen from '../../functions/keyGen';
-
 // data
 import theme from '../../json/theme.json';
 
+const { gray3 } = theme.colors;
+
+const DefaultMessage = styled.p`
+  font-weight: 500;
+  color: ${gray3};
+`;
+
+const Default = () => {
+  return (
+    <div>
+      <DefaultMessage>尚未報名任何課程</DefaultMessage>
+    </div>
+  );
+};
+
 const ClassList = () => {
-  const { userClasses, rescheduleClasses } = useContext(userStatusContext);
+  const { userClasses } = useContext(userStatusContext);
   return (
     <div className="container-fluid px-0">
       <div className="row">
@@ -24,12 +34,11 @@ const ClassList = () => {
           <div className="col-12 col-md-6">
             <UserClassList classes={userClasses} />
           </div>
-        ) : null}
-        {rescheduleClasses.length ? (
-          <div className="col-12 col-md-6">
-            <UserClassList classes={rescheduleClasses} />
+        ) : (
+          <div className="col-12">
+            <Default />
           </div>
-        ) : null}
+        )}
       </div>
     </div>
   );
