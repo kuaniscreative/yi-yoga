0f4ab629c5d431a54a12894187dfe8ed6853aa80
Kuanisinajar <superbenben@livemail.tw>
2020-03-23 23:33:52 +0800

    [Add] userStatus payment single

--- src/components/users/userStatus_paymentSingle.js
+++ src/components/users/userStatus_paymentSingle.js
@@ -1,48 +1,79 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const PaymentSingle = ({ infos }) => {
-    const status = infos.moneyReceived
-        ? "finished"
-        : infos.moneySent
-        ? "confirmPending"
-        : "unpaid";
-    const statusOuput = () => {
-        switch (status) {
-            case "unpaid":
-                return (
-                    <ul className="comfyList">
-                        <li>
-                            未繳費。如果已繳費，
-                            <Link to={`/payment/${infos.id}`}>
-                            <span className="action">點此通知芝伊</span>
-                            </Link>
-                        </li>
-                    </ul>
-                );
-            case "confirmPending":
-                return (
-                    <ul className="comfyList">
-                        <li>已繳費，等待芝伊確認</li>
-                    </ul>
-                );
-            case "finished":
-                return (
-                    <ul className="comfyList">
-                        <li>已完成，放心上課囉～</li>
-                    </ul>
-                );
-            default:
-                return null;
-        }
-    };
+// data
+import theme from '../../json/theme';
 
-    return (
-        <div className="paymentSingle">
-            <p name="title">{infos.sessionName}</p>
-            {statusOuput()}
-        </div>
-    );
+const { gray1, gray4 } = theme.colors;
+
+const PaymentContainer = styled.div`
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid ${gray1};
+`;
+
+const PaymentTitle = styled.div`
+  font-size: 1rem;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
+  letter-spacing: normal;
+`;
+
+const PaymentStatus = styled.div`
+  font-size: 1rem;
+  font-weight: 400;
+  color: ${gray4};
+  letter-spacing: normal;
+`;
+
+const ListItem = styled.li`
+  font-size: 1rem;
+  font-weight: 400;
+  color: ${gray4};
+  letter-spacing: normal;
+`;
+
+const ActionText = styled.u`
+  font-size: 1rem;
+  font-weight: 400;
+  color: ${gray4};
+  letter-spacing: normal;
+`;
+
+const PaymentSingle = ({ payment }) => {
+  const status = payment.moneyReceived
+    ? 'finished'
+    : payment.moneySent
+    ? 'paid'
+    : 'pending';
+
+  return (
+    <PaymentContainer>
+      <PaymentTitle>{payment.sessionName}</PaymentTitle>
+      <PaymentStatus>
+        {status === 'finished' ? (
+          <ul className="comfyList">
+            <ListItem>已完成</ListItem>
+          </ul>
+        ) : null}
+        {status === 'paid' ? (
+          <ul className="comfyList">
+            <ListItem>已繳費，等待芝伊確認</ListItem>
+          </ul>
+        ) : null}
+        {status === 'pending' ? (
+          <ul className="comfyList">
+            <ListItem>
+              未繳費 —— 如果已繳費，
+              <Link to={`/payment/${payment.id}`}>
+                <ActionText>點此通知芝伊</ActionText>
+              </Link>
+            </ListItem>
+          </ul>
+        ) : null}
+      </PaymentStatus>
+    </PaymentContainer>
+  );
 };
 
 export default PaymentSingle;
--- src/components/users/userStatus_payments.js
+++ src/components/users/userStatus_payments.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// components
+import PaymentSingle from './userStatus_paymentSingle';
+
+// context
+import { userStatusContext } from '../contexts/userStatusContext';
+
+// function
+import keyGen from '../../functions/keyGen';
 
 const Payments = () => {
-  return <div>繳費狀態</div>;
+  /** Get payments from context */
+  const { payments } = useContext(userStatusContext);
+
+  return (
+    <div>
+      {payments.map((payment) => {
+        return <PaymentSingle payment={payment} key={keyGen()} />;
+      })}
+    </div>
+  );
 };
 
 export default Payments;
