2ff23c46fbadf4ce3eaa2d4ebe1efa390bbf6412
Kuanisinajar <superbenben@livemail.tw>
2020-05-18 22:09:30 +0800

    Add auto reschedule

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const nodemailer = require('nodemailer');
 const Email = require('email-templates');
-const cors = require('cors')({ origin: true });
 admin.initializeApp(functions.config().firebase);
 
 const account = functions.config().gmail.account;
@@ -153,4 +151,117 @@ exports.sendNewStudentNotification = functions.firestore
         url: 'https://class-manage-80e60.firebaseapp.com/',
       },
     });
+  });
+
+function updateClassProfile(
+  classId,
+  pendingStudent,
+  pendingStudents,
+  rescheduleStudents
+) {
+  const { id, name, nickName, email } = pendingStudent;
+  const newPendingStudents = pendingStudents.filter((stu) => {
+    return stu.id !== id;
+  });
+  const newRescheduleStudents = rescheduleStudents.slice(0);
+  newRescheduleStudents.push({
+    id,
+    name,
+    nickName,
+    email,
+  });
+  return admin.firestore().collection('classProfile').doc(classId).update({
+    pendingStudents: newPendingStudents,
+    rescheduleStudents: newRescheduleStudents,
+  });
+}
+
+function updateLeaveRecord(pendingClassId, studentId) {
+  return admin
+    .firestore()
+    .collection('leaveRecord')
+    .doc(studentId)
+    .get()
+    .then((snap) => {
+      const record = snap.data();
+      const target = record.reschedulePending.find((item) => {
+        return item.pendingClassId === pendingClassId;
+      });
+      const newReschedulePending = record.reschedulePending.filter((item) => {
+        return item.pendingClassId !== pendingClassId;
+      });
+      const newRescheduled = record.rescheduled;
+      newRescheduled.push({
+        leaveDate: target.leaveDate,
+        rescheduleClassId: pendingClassId,
+      });
+
+      return admin.firestore().collection('leaveRecord').doc(studentId).update({
+        reschedulePending: newReschedulePending,
+        rescheduled: newRescheduled,
+      });
+    });
+}
+
+exports.movePendingStudnet = functions.firestore
+  .document('classProfile/{classId}')
+  .onUpdate((change, context) => {
+    const classId = context.params.classId;
+    const after = change.after.data();
+    const isAvailable =
+      after.opacity > after.students.length + after.rescheduleStudents.length;
+    const hasPendingStudent = !!after.pendingStudents.length;
+    if (isAvailable && hasPendingStudent) {
+      const currentTime = new Date();
+      const classTime = after.date.toDate();
+      const classStartWithinTwoHour = classTime - currentTime < 7200000;
+      const pendingStudent = after.pendingStudents[0];
+      if (classStartWithinTwoHour) {
+        return functions.firestore.sendQueryMail({
+          studentId: pendingStudent.id,
+          classId: classId,
+          dateString: after.name,
+          startAt: after.type,
+        });
+      } else {
+        const tasks = [
+          updateClassProfile(
+            classId,
+            pendingStudent,
+            after.pendingStudents,
+            after.rescheduleStudents
+          ),
+          updateLeaveRecord(classId, pendingStudent.id),
+        ];
+        return Promise.all(tasks).then(() => {
+          const email = new Email({
+            message: {
+              from: '芝伊瑜珈 <yiyoga.official@gmail.com>',
+            },
+            // uncomment below to send emails in development/test env:
+            // send: true,
+            transport: {
+              service: 'gmail',
+              auth: {
+                user: account,
+                pass: password,
+              },
+            },
+          });
+
+          return email.send({
+            template: 'rescheduleSuccess',
+            message: {
+              to: pendingStudent.email,
+            },
+            locals: {
+              name: pendingStudent.name,
+              date: after.name,
+              time: after.type,
+            },
+          });
+        });
+      }
+    }
+    return;
   });
